ca0398191830cb0b2c701b8e869b31d342e7e829
jroltgen <jroltgen@68a92380-eb83-11de-9298-fbd6a473cdd1>
2009-12-22 17:05:30 +0000

    More basic GUI stuff - the app still looks bad, but it's getting closer.

--- RoboticGardenClient/src/garden/Planter.java
+++ RoboticGardenClient/src/garden/Planter.java
@@ -2,9 +2,12 @@ package garden;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
+import garden.comm.Plant;
+import garden.comm.PlanterState;
 import garden.widgets.*;
 
 import javax.swing.JLabel;
@@ -35,14 +38,41 @@ public class Planter extends JPanel {
 	private void initPanel() {
 		// TODO Auto-generated method stub
 		setSize(WIDTH, HEIGHT);
+		setLayout(null);
+		
+		planterText = new JLabel("Unnamed Planter");
+		planterText.setLocation(80, 20);
+		planterText.setFont(new Font(Font.SERIF, Font.BOLD, 18));
+		planterText.setSize(WIDTH - 20, 15);
+		add(planterText);
+		
+		_plantTypeDisplay = new PlantTypeDisplay();
+		_plantTypeDisplay.setPlantType(Plant.Type.RADISH);
+		_plantTypeDisplay.setLocation(20, 20);
+		_plantTypeDisplay.setSize(50, 50);
+		add(_plantTypeDisplay);
+		
+		JLabel gdhLabel = new JLabel("GDH Progress: ");
+		gdhLabel.setLocation(80, 50);
+		gdhLabel.setFont(new Font(Font.SERIF, Font.PLAIN, 14));
+		gdhLabel.setSize(WIDTH - 50, 15);
+		add(gdhLabel);
 	}
 	
 	@Override
 	protected void paintComponent(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
+
+		g2.setColor(Color.GRAY);
+		g2.fillRect(0, 0, getWidth(), getHeight());
 		g2.setColor(Color.BLACK);
 		g2.setStroke(new BasicStroke(5));
 		g2.drawRect(0, 0, getWidth(), getHeight());
+	}
+	
+	public void updateState(PlanterState s) {
+		planterText.setText("Planter " + s.getID());
+		
 	}
 	
 	
--- RoboticGardenClient/src/garden/widgets/PlantTypeDisplay.java
+++ RoboticGardenClient/src/garden/widgets/PlantTypeDisplay.java
@@ -1,5 +1,14 @@
 package garden.widgets;
 
+import garden.comm.Plant;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 
 public class PlantTypeDisplay extends JComponent {
@@ -8,5 +17,31 @@ public class PlantTypeDisplay extends JComponent {
 	 * 
 	 */
 	private static final long serialVersionUID = -8379315439099796742L;
+
+	private BufferedImage _image;
+	private Plant.Type type;
+
+	public PlantTypeDisplay() {
+		try {
+			_image = ImageIO.read(new File("images/title.png"));
+		} catch (IOException e) {
+			System.err.println("Problem reading file images/title.png");
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		g2.drawImage(_image, 0, 0, getWidth(), getHeight(), 0, 0,
+				_image.getWidth(), _image.getHeight(), null);
+	}
+	
+	public void setPlantType(Plant.Type t) {
+		type = t;
+		_image = Plant.getImage(type);
+		repaint();
+	}
 
 }
