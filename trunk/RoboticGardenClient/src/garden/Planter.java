d191f17a41c62b95f1a343bd8d2b2bab45e6a63d
jroltgen <jroltgen@68a92380-eb83-11de-9298-fbd6a473cdd1>
2009-12-22 16:53:56 +0000

    Basic GUI starting to come together, working on individual widgets next.

--- trunk/RoboticGardenClient/src/garden/Planter.java
+++ trunk/RoboticGardenClient/src/garden/Planter.java
@@ -1,5 +1,13 @@
 package garden;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import garden.widgets.*;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Planter extends JPanel {
@@ -8,5 +16,34 @@ public class Planter extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = -127271935278457306L;
+	
+	public static int WIDTH = 400;
+	public static int HEIGHT = 200;
 
+	private PlantTypeDisplay _plantTypeDisplay;
+	private GdhDisplay _gdhDisplay;
+	private MoistureDisplay _moistureDisplay;
+	private TemperatureDisplay _temperatureDisplay;
+	private WeatherDisplay _weatherDisplay;
+	private int planterID;
+	private JLabel planterText;
+	
+	public Planter() {
+		initPanel();
+	}
+
+	private void initPanel() {
+		// TODO Auto-generated method stub
+		setSize(WIDTH, HEIGHT);
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setColor(Color.BLACK);
+		g2.setStroke(new BasicStroke(5));
+		g2.drawRect(0, 0, getWidth(), getHeight());
+	}
+	
+	
 }
