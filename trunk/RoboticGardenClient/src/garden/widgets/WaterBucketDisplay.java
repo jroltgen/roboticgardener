d17340c2e48f544ba25045c5590cee30b69f5555
jroltgen <jroltgen@68a92380-eb83-11de-9298-fbd6a473cdd1>
2009-12-30 14:43:20 +0000

    Added the water bucket display, working on making it look just a bit better for its photoshoot

---
+++ trunk/RoboticGardenClient/src/garden/widgets/WaterBucketDisplay.java
@@ -0,0 +1,58 @@
+package garden.widgets;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JComponent;
+
+public class WaterBucketDisplay extends JComponent {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2229802526482580933L;
+	
+	private boolean isFull;
+	
+	public WaterBucketDisplay(boolean level) {
+		this.isFull = level;
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		
+		// Draw background.
+		g2.setColor(Color.GRAY);
+		g2.fillRect(0, 0, getWidth(), getHeight());
+		
+		// Draw progress
+		g2.setColor(Color.BLUE);
+		int height;
+		if (isFull) height = (int) (getHeight() * 0.90);
+		else height = (int) (getHeight() * 0.1);
+		g2.fillRect(0, getHeight() - height, getWidth(), getHeight());
+		
+		// Draw border.
+		g2.setColor(Color.BLACK);
+		g2.setStroke(new BasicStroke(5));
+		g2.drawRect(0, 0, getWidth() - 2, getHeight() - 2);
+		
+		g2.rotate(-Math.PI / 2);
+		g2.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 30));
+		g2.drawString("WATER LEVEL", -getHeight() / 2 - 100, getWidth() / 2 + 10);
+	}
+	
+	public void setEmpty() {
+		this.isFull = false;
+		repaint();
+	}
+	
+	public void setFull() {
+		this.isFull = true;
+		repaint();
+	}
+}
